23a2ebbedb0bd2100f2ecff5fa12336ed6732a90
Mayank Swaraj <mayankswaraj18cr@gmail.com>
2026-09-11 19:27:18 +0530

    Add Google OAuth2 client configuration

---
+++ src/config/googleAuth.js
@@ -0,0 +1,19 @@
+const { google } = require('googleapis');
+
+const createOAuth2Client = () => {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+  );
+
+  if (process.env.GOOGLE_REFRESH_TOKEN) {
+    oauth2Client.setCredentials({
+      refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+    });
+  }
+
+  return oauth2Client;
+};
+
+module.exports = createOAuth2Client;
